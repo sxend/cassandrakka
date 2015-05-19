bbf922e1ee4c66222c6ca3194d6f92b7862016cc
sxend <arimitsu.k+github@gmail.com>
2015-05-20 01:31:20 +0900

    add type

--- src/main/scala/arimitsu/sf/cassandrakka/Cassandrakka.scala
+++ src/main/scala/arimitsu/sf/cassandrakka/Cassandrakka.scala
@@ -1,13 +1,13 @@
 package arimitsu.sf.cassandrakka
 
-import akka.actor.{Props, ActorSystem}
+import akka.actor.{ActorRef, Props, ActorSystem}
 import arimitsu.sf.cassandrakka.actors.{ConfigurationManager, ClusterManager}
 
 object Cassandrakka{
   def apply()(implicit system: ActorSystem): Cassandrakka = {
     val confugurationManager = system.actorOf(Props(classOf[ConfigurationManager]))
     system.actorOf(Props(classOf[ClusterManager], new {
-      val configurationManager = configurationManager
+      val configurationManager:ActorRef = configurationManager
     }))
     new Cassandrakka {
       override def withCql(f: => (Session) => Unit): Unit = {
