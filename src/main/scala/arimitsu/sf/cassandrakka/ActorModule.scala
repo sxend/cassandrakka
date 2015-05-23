9acef41bea27f4a23ec9cb135a7a7f93c2ac79b8
sxend <arimitsu.k+github@gmail.com>
2015-05-23 18:25:33 +0900

    add actor binding class

---
+++ src/main/scala/arimitsu/sf/cassandrakka/ActorModule.scala
@@ -0,0 +1,28 @@
+package arimitsu.sf.cassandrakka
+
+import akka.actor._
+import akka.util._
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.reflect.ClassTag
+
+class ActorModule[A <: Actor](props: => Props, name: => String = "")(implicit system: ActorSystem) {
+  def toActorRef: ActorRef = name match {
+    case "" => system.actorOf(props)
+    case _ => system.actorOf(props, name)
+  }
+}
+
+object ActorModule {
+
+  trait Mapping[M <: Actor, A, B] {
+  }
+
+  implicit class TypedAsk[M <: Actor](module: ActorModule[M]) {
+
+    import akka.pattern.ask
+
+    def typedAsk[A, B](message: => A)(implicit mapping: Mapping[M, A, B], ec: ExecutionContext, timeout: Timeout, tag: ClassTag[B]): Future[B] = module.toActorRef.ask(message).mapTo[B]
+  }
+
+}
